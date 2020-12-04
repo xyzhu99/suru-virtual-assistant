e543fdd9f1b341f0d2441deb13abc4c3a67c43e6
Xuyuan Zhu <zhuxuyuan1999@hotmail.com>
2020-12-03 22:45:39 -0500

    respond for time and search

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import speech_recognition as sr
+from time import ctime
+import webbrowser
 
 recog = sr.Recognizer()
 
-def record_audio(): 
+def record_audio(ask = False): 
     with sr.Microphone() as source: 
+        if ask: 
+            print(ask)
         audio = recog.listen(source)
         voice_data = ''
         try: 
@@ -17,6 +21,13 @@ def record_audio():
 def respond(voice_data): 
     if 'what is your name' in voice_data: 
         print('My name is Suru')
+    if 'what time is it' in voice_data: 
+        print(ctime())
+    if 'search' in voice_data: 
+        search = record_audio('What do you want to search? ')
+        url = 'https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        print('Here is what I found for ' + search)
 
 
 print('How can I help you? ')
